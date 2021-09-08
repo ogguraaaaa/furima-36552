4bf98717e21dff9f5e43ef81ccb13a5969ed5b82
ogguraaaaa <o7o1464o5o2.ogura@gmail.com>
2021-09-08 12:40:20 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,12 +5,20 @@ class ItemsController < ApplicationController
   def new
    @item = Item.new
   end
+
+  def create
+    @item = Item.new(user_params)
+    if @item.save
+      redrect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def item_params
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  end
+
 end
-    #def create
-    #  @item = Item.new(user_params)
-    #  if @item.save
-    #    redrect_to root_path
-    #  else
-    #    render 'new'
-    #  end
-    #end
+
+
