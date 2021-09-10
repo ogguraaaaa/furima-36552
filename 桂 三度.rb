3706808b6841c5c3a3980a879fbe0ece63bb75e9
ogguraaaaa <o7o1464o5o2.ogura@gmail.com>
2021-09-10 17:35:01 +0900

    商品一覧表示機能の実装条件完了

---
+++ 桂 三度.rb
@@ -0,0 +1,16 @@
+def everything_that_happens_twice_will_surely_happen_a_third_time
+num = 1
+  while num <= 41 do
+    if num.to_s.include?("3") || num % 3 == 0
+      puts "#{num}っ！！！"
+    elsif num == 41
+      puts "ぁおもろぉーぅ！"
+    else
+      puts num
+    end
+  num = num + 1
+  end
+end
+
+everything_that_happens_twice_will_surely_happen_a_third_time
+
