7ec52ac60ec0d66a61e9388cd0523cd3e56ffe2f
ogguraaaaa <o7o1464o5o2.ogura@gmail.com>
2021-09-15 13:20:41 +0900

    修正1

--- app/models/order_purchase_history.rb
+++ app/models/order_purchase_history.rb
@@ -4,7 +4,7 @@ class OrderPurchaseHistory
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :number, format: { with: /\A[0-9]{,11}\z/, message: 'is invalid. Input half-width characters.' }
+    validates :number, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid. Input half-width characters.' }
     validates :city
     validates :address
     validates :user_id
